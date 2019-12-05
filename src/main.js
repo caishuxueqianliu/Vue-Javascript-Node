86a517ea1e16f30a5a9c426f76f116732a623ee4
caishuxueqianliu <470290171@qq.com>
2019-12-06 00:16:27 +0800

    ‘v1‘

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
+
 Vue.use(MintUI)
 // 1. 导入 ant-design-vue 组件库
 import Antd from 'ant-design-vue'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,75 +1,65 @@
 <template>
   <div id="app">
-    <a-input placeholder="请输入任务" class="my_ipt" />
-    <a-button type="primary">添加事项</a-button>
-
-    <a-list bordered :dataSource="list" class="dt_list">
-      <a-list-item slot="renderItem" slot-scope="item">
-        <!-- 复选框 -->
-        <a-checkbox>{{item.info}}</a-checkbox>
-        <!-- 删除链接 -->
-        <a slot="actions">删除</a>
-      </a-list-item>
-
-      <!-- footer区域 -->
-      <div slot="footer" class="footer">
-        <!-- 未完成的任务个数 -->
-        <span>0条剩余</span>
-        <!-- 操作按钮 -->
-        <a-button-group>
-          <a-button type="primary">全部</a-button>
-          <a-button>未完成</a-button>
-          <a-button>已完成</a-button>
-        </a-button-group>
-        <!-- 把已经完成的任务清空 -->
-        <a>清除已完成</a>
+    <div>
+      
+         <el-row :gutter="0">
+  <el-col :xs="8" :sm="6" :md="4" :lg="2" :xl="2"><div class="grid-content bg-purple">1</div></el-col>
+<el-col :xs="4" :sm="6" :md="8" :lg="10" :xl="10"><div class="grid-content bg-purple-light">4</div></el-col>
+  <el-col :xs="4" :sm="6" :md="8" :lg="2" :xl="10"><div class="grid-content bg-purple">3</div></el-col>
+  <el-col :xs="8" :sm="6" :md="4" :lg="10" :xl="2"><div class="grid-content bg-purple-light">4</div></el-col>
+</el-row>
+    </div>
+    <div class="container">
+<!-- Stack the columns on mobile by making one full-width and the other half-width -->
+<div class="row">
+  <div class="col-xs-4 col-sm-3 col-md-2 col-xl-1 col-1"><div class="grid-content bg-purple">1</div></div>
+  <div class="col-xs-2 col-sm-3 col-md-4 col-xl-5 col-5"><div class="grid-content bg-purple-light">1</div></div>
+    <div class="col-xs-2 col-sm-3 col-md-4 col-xl-1 col-5"><div class="grid-content bg-purple">1</div></div>
+      <div class="col-xs-4 col-sm-3 col-md-2 col-xl-5 col-1"><div class="grid-content bg-purple-light">1</div></div>
+</div>
       </div>
-    </a-list>
+
+
+
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'app',
   data() {
     return {
-      list: [
-        {
-          id: 0,
-          info: 'Racing car sprays burning fuel into crowd.',
-          done: false
-        },
-        { id: 1, info: 'Japanese princess to wed commoner.', done: false },
-        {
-          id: 2,
-          info: 'Australian walks 100km after outback crash.',
-          done: false
-        },
-        { id: 3, info: 'Man charged over missing wedding girl.', done: false },
-        { id: 4, info: 'Los Angeles battles huge wildfires.', done: false }
-      ]
     }
   }
 }
 </script>
 
 <style scoped>
-#app {
-  padding: 10px;
-}
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .no-gutters {
+  margin-right: 0;
+  margin-left: 0;
 
-.my_ipt {
-  width: 500px;
-  margin-right: 10px;
+  > .col,
+  > [class*="col-"] {
+    padding-right: 0;
+    padding-left: 0;
+  }
 }
-
-.dt_list {
-  width: 500px;
-  margin-top: 10px;
-}
-
-.footer {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+</style>
