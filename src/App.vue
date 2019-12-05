8fecd6b97d6b064cebd3e005f2cff9f7fea3a589
caishuxueqianliu <470290171@qq.com>
2019-12-05 23:10:45 +0800

    ‘v1‘

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  
+     
+
+
+
+   
+<router-view> </router-view>
+
+
+
+
+  <div id="nav">
+  
+<mt-tabbar >
+     <router-link to="/home">
+  <mt-tab-item id="1">
+    <img slot="icon" >
+   1
+  </mt-tab-item>
+
+ </router-link> 
+  <router-link to="/about">
+  <mt-tab-item id="2">
+    <img slot="icon" >
+   2
+  </mt-tab-item>
+   </router-link> 
+  <mt-tab-item id="3">
+    <img slot="icon" >
+    3
+  </mt-tab-item>
+  <mt-tab-item id="4">
+    <img slot="icon" >
+    4
+  </mt-tab-item>
+</mt-tabbar>    
     </div>
-    <router-view/>
+
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+
