000d77e02cdd0562a556dbe71b0467327a80203c
caishuxueqianliu <470290171@qq.com>
2019-12-20 22:30:28 +0800

    1

--- src/node/02.js
+++ src/node/02.js
@@ -73,52 +73,72 @@
  // 2. 如果是目录，读取渲染目录列表
  // 3. 如果目录并且有该目录中有 index.html 则直接渲染目录中的 index.html
 
-var http = require('http')
+// var http = require('http')
+// var template = require('art-template')
+// var path = require('path')
+// var server = http.createServer()
+// var wwwDir = 'D:/Movie/www'
 
-var template = require('art-template')
-var path = require('path')
-var server = http.createServer()
-var wwwDir = 'D:/Movie/www'
+// server.on('request',  (req,res)=> {
 
-server.on('request',  (req,res)=> {
+//   var url = req.url
+//   var url='.'+url
+//   var urlPath = path.join(wwwDir, url)
+// 路径拼接问题  ？？？？？？？？？？？？
+// //获取完整路径
+// console.log(wwwDir)
+// console.log(url)
+// console.log(urlPath)
+// var fs = require('fs')
+//   fs.stat(url, (err, stats) =>{
+//     if (err) {
+//       return res.end('404 ')
+//     }
+//     if (stats.isFile()) {
+//       fs.readFile(url,  (err, data)=>{
+//         if (err) {
+//           return res.end('404 Not Found.')
+//         }
+       
+//         res.setHeader('Content-Type','text/plain;charset=utf-8')
+//         res.end(data)
+//       })
+//     } else if (stats.isDirectory()) {
+//       var templateStr = fs.readFileSync('./static-template.html').toString()
+//       var files = fs.readdirSync(url)
+//       // var data = files.map(function (item) {
+//       //   return {
+//       //     url: 
+//       //     name: 
+//       //     type: 
+//       //   }
+//       // })
 
-  var url = req.url
-  var urlPath = path.join(wwwDir, url)
-//获取完整路径
-console.log(wwwDir)
-console.log(url)
-console.log(urlPath)
-var fs = require('fs')
-  fs.stat(url, (err, stats) =>{
-    if (err) {
-      return res.end('404 ')
-    }
-    if (stats.isFile()) {
-      fs.readFile(url,  (err, data)=>{
-        if (err) {
-          return res.end('404 Not Found.')
-        }
-        res.end(data)
-      })
-    } else if (stats.isDirectory()) {
-      var templateStr = fs.readFileSync('./static-template.html').toString()
-      var files = fs.readdirSync(url)
-      // var data = files.map(function (item) {
-      //   return {
-      //     url: 
-      //     name: 
-      //     type: 
-      //   }
-      // })
+
+//       var htmlStr = template.render(templateStr, {
+//         files: files
+//       })
+//       res.end(htmlStr)
+//     }
+//   })
+// })
 
 
-      var htmlStr = template.render(templateStr, {
-        files: files
-      })
-      res.end(htmlStr)
-    }
-  })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
